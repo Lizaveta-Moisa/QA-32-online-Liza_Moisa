689b8ced4d49755015be0c7ad7fcd4e429200cd7
Lizavete-Moisa <sliza1882@gmail.com>
2026-03-02 23:52:49 +0300

    task-21

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,18 @@
+import org.example.Calculator;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest {
+    protected Calculator calculator;
+
+    @BeforeMethod
+    public void setUp() {
+        calculator = new Calculator();
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        calculator = null;
+    }
+}
