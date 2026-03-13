5230e5e1fc5a4f2382a244551117e019629541dd
AGSR\mey <sliza1882@gmail.com>
2026-03-13 15:46:17 +0300

    add new task-23

---
+++ src/main/java/pages/checkout/CheckoutPage.java
@@ -0,0 +1,60 @@
+package pages.checkout;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.BasePage;
+
+public class CheckoutPage extends BasePage {
+    public static final String TITLE_TEXT = "Checkout: Your Information";
+
+    private static final String URL = "https://www.saucedemo.com/checkout-step-one.html";
+
+    private static final By FIRST_NAME_FIELD = By.id("first-name");
+    private static final By LAST_NAME_FIELD = By.id("last-name");
+    private static final By POSTAL_CODE_FIELD = By.id("postal-code");
+    private static final By CONTINUE_BUTTON = By.id("continue");
+    private static final By CANCEL_BUTTON = By.id("cancel");
+
+    public CheckoutPage(WebDriver driver,
+                        WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    @Override
+    public BasePage waitForLoad() {
+        wait.until(ExpectedConditions.elementToBeClickable(CONTINUE_BUTTON));
+        return this;
+    }
+
+    public CheckoutPage open() {
+        driver.get(URL);
+        return this;
+    }
+
+    public CheckoutPage typeFirstName(String firstName) {
+        driver.findElement(FIRST_NAME_FIELD).sendKeys(firstName);
+        return this;
+    }
+
+    public CheckoutPage typeLastName(String lastName) {
+        driver.findElement(LAST_NAME_FIELD).sendKeys(lastName);
+        return this;
+    }
+
+    public CheckoutPage typePostalCode(String postalCode) {
+        driver.findElement(POSTAL_CODE_FIELD).sendKeys(postalCode);
+        return this;
+    }
+
+    public void clickContinue() {
+        driver.findElement(CONTINUE_BUTTON).click();
+    }
+
+    public String getErrorMessage() {
+        WebElement errorElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test='error']")));
+        return errorElement.getText();
+    }
+}
---
+++ src/main/java/pages/login/LoginPage.java
@@ -0,0 +1,52 @@
+package pages.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.BasePage;
+
+public class LoginPage extends BasePage {
+    public static final String TITLE_TEXT = "Swag Labs";
+
+    private static final String URL = "https://www.saucedemo.com/";
+
+    private static final By USERNAME_FIELD = By.id("user-name");
+    private static final By PASSWORD_FIELD = By.id("password");
+    private static final By LOGIN_BUTTON = By.id("login-button");
+    private static final By ERROR_LOCATOR = By.cssSelector("[data-test='error']");
+
+    public LoginPage(WebDriver driver,
+                     WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    @Override
+    public LoginPage waitForLoad() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(LOGIN_BUTTON));
+        return this;
+    }
+
+    public LoginPage open() {
+        driver.get(URL);
+        return this;
+    }
+
+    public LoginPage typeUserName(String username) {
+        driver.findElement(USERNAME_FIELD).sendKeys(username);
+        return this;
+    }
+
+    public LoginPage typePassword(String password) {
+        driver.findElement(PASSWORD_FIELD).sendKeys(password);
+        return this;
+    }
+
+    public String getErrorMessage(){
+        return driver.findElement(ERROR_LOCATOR).getText();
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(LOGIN_BUTTON).click();
+    }
+}
