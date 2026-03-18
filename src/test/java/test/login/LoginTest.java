cef114e36a52c5c4df53f10676acb91f8ad38558
Lizavete-Moisa <sliza1882@gmail.com>
2026-03-18 22:45:06 +0300

    changed task-26

--- src/test/java/test/login/LoginTest.java
+++ src/test/java/test/login/LoginTest.java
@@ -40,6 +40,7 @@ public class LoginTest extends test.BaseTest {
         assertThat(productsPage.isLoaded())
                 .as("Пользователь не авторизовался и не попал на страницу товаров")
                 .isTrue();
+
         logger.info("=== END TEST: SUCCESS ===");
     }
 
@@ -49,81 +50,110 @@ public class LoginTest extends test.BaseTest {
         logger.info("=== START TEST: loginWithoutUsername ===");
         LoginPage loginPage = new LoginPage(driver, wait);
 
+        logger.info("Открываем страницу логина");
         loginPage.open()
-                .waitForLoad()
-                .typePassword(PASS_WORD)
+                .waitForLoad();
+
+        logger.info("Вводим только пароль и нажимаем кнопку логина");
+        loginPage.typePassword(PASS_WORD)
                 .clickLoginButton();
 
         logger.info("Делаем скриншот");
         ScreenshotUtil.takeScreenshot(driver);
 
+        logger.info("Получаем текст ошибки");
         String errorMessage = loginPage.getErrorMessage();
 
+        logger.info("Проверяем текст ошибки");
         assertThat(errorMessage)
                 .as("Ошибка отсутствует или некорректная")
                 .isEqualTo("Epic sadface: Username is required");
+
         logger.info("=== END TEST: SUCCESS ===");
     }
 
     @Description("Проверка сообщения об ошибке при отсутствии пароля")
     @Test
     public void loginWithoutPassword() {
-
+        logger.info("=== START TEST: loginWithoutPassword ===");
         LoginPage loginPage = new LoginPage(driver, wait);
 
+        logger.info("Открываем страницу логина");
         loginPage.open()
-                .waitForLoad()
-                .typeUserName(USER_NAME)
+                .waitForLoad();
+
+        logger.info("Вводим только имя пользователя и нажимаем кнопку логина");
+        loginPage.typeUserName(USER_NAME)
                 .clickLoginButton();
 
+        logger.info("Делаем скриншот");
         ScreenshotUtil.takeScreenshot(driver);
 
+        logger.info("Получаем текст ошибки");
         String errorMessage = loginPage.getErrorMessage();
 
+        logger.info("Проверяем текст ошибки");
         assertThat(errorMessage)
                 .as("Ошибка отсутствует или некорректна")
                 .isEqualTo("Epic sadface: Password is required");
+
+        logger.info("=== END TEST: SUCCESS ===");
     }
 
     @Description("Проверка сообщения об ошибке при неверных учетных данных")
     @Test
     public void loginWithInvalidCredentials() {
-
+        logger.info("=== START TEST: loginWithInvalidCredentials ===");
         LoginPage loginPage = new LoginPage(driver, wait);
 
         final String userName = "UserTest";
         final String password = "PasswordTest";
 
+        logger.info("Открываем страницу логина");
         loginPage.open()
-                .waitForLoad()
-                .typeUserName(userName)
+                .waitForLoad();
+
+        logger.info("Вводим невалидные логин и пароль");
+        loginPage.typeUserName(userName)
                 .typePassword(password)
                 .clickLoginButton();
 
+        logger.info("Делаем скриншот");
         ScreenshotUtil.takeScreenshot(driver);
 
+        logger.info("Получаем текст ошибки");
         String errorMessage = loginPage.getErrorMessage();
 
+        logger.info("Проверяем текст ошибки");
         assertThat(errorMessage)
                 .as("Ошибка отсутствует или некорректна")
                 .isEqualTo("Epic sadface: Username and password do not match any user in this service");
+
+        logger.info("=== END TEST: SUCCESS ===");
     }
 
     @Description("Проверка соответствия заголовка страницы")
     @Test
     public void pageTitle() {
+        logger.info("=== START TEST: pageTitle ===");
 
         LoginPage loginPage = new LoginPage(driver, wait);
 
+        logger.info("Открываем страницу логина");
         loginPage.open()
                 .waitForLoad();
 
+        logger.info("Делаем скриншот");
         ScreenshotUtil.takeScreenshot(driver);
 
+        logger.info("Получаем заголовок страницы");
         String title = driver.getTitle();
 
+        logger.info("Проверяем заголовок страницы");
         assertThat(title)
                 .as("Заголовок не соответствует заголовку страницы")
                 .isEqualTo(TITLE_TEXT);
+
+        logger.info("=== END TEST: SUCCESS ===");
     }
 }
